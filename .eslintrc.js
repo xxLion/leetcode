5318aa1a3c8ce55089f006a6e989f7f48864e5a5
chenshiqi <chenshiqi1419@emao.com>
2019-03-13 19:25:21 +0800

    单词反转

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,10 @@
 module.exports = {
-  "extends": ["standard","plugin:jest/recommended"]
+  "extends": ["airbnb-base","plugin:jest/recommended"],
+  rules: {
+    "linebreak-style": [0,"error", "windows"],
+    "no-plusplus": 0,
+    "func-names": 0,
+    "no-console": 0,
+    "semi": "off"
+  }
 };
---
+++ test/string/day1.test.js
@@ -0,0 +1,5 @@
+import reversrWorld from '../../code/string/day1'
+
+test('expect success', () => {
+  expect(reversrWorld("Let's take LeetCode contest")).toBe("s'teL ekat edoCteeL tsetnoc")
+})
